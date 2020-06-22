52825ea19a2c58c4686dea4e6676a040c54b8afa
K.S.KIM <nullable@kakao.com>
2020-06-22 19:36:42 +0900

    refactor: LottoNumber 캐싱 적용

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,16 +1,26 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class LottoNumber {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
+
     private final int number;
 
-    public LottoNumber(final int number) {
+    private LottoNumber(final int number) {
         validateNumberRange(number);
         this.number = number;
     }
 
-    private void validateNumberRange(int number) {
+    public static LottoNumber valueOf(final int number) {
+        validateNumberRange(number);
+        return LottoNumberCache.get(number);
+    }
+
+    private static void validateNumberRange(final int number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new InvalidLottoNumberException("로또 수의 범위가 올바르지 않습니다: " + number);
         }
@@ -18,5 +28,34 @@ public class LottoNumber {
 
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        LottoNumber that = (LottoNumber)o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    private static class LottoNumberCache {
+        private static final Map<Integer, LottoNumber> cache = new HashMap<>();
+
+        static {
+            for (int number = MIN_NUMBER; number <= MAX_NUMBER; ++number) {
+                cache.put(number, new LottoNumber(number));
+            }
+        }
+
+        static LottoNumber get(int number) {
+            return cache.get(number);
+        }
     }
 }
--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,15 +13,39 @@ class LottoNumberTest {
     @ValueSource(ints = {1, 20, 45})
     @ParameterizedTest
     void constructor(final int number) {
-        assertThat(new LottoNumber(number)).isInstanceOf(LottoNumber.class);
+        assertThat(LottoNumber.valueOf(number)).isInstanceOf(LottoNumber.class);
     }
 
     @DisplayName("로또 숫자의 범위를 벗어났을 때 예외 발생")
     @ValueSource(ints = {-1, 46})
     @ParameterizedTest
     void constructor_NumberRangeOutOfBound_ExceptionThrown(final int number) {
-        assertThatThrownBy(() -> new LottoNumber(number))
+        assertThatThrownBy(() -> LottoNumber.valueOf(number))
                 .isInstanceOf(InvalidLottoNumberException.class)
                 .hasMessageContaining("로또 수의 범위가 올바르지 않습니다");
+    }
+
+    @DisplayName("같은 값을 가지는 로또 객체는 논리적 동치성을 가짐")
+    @Test
+    void equals_equalNumber() {
+        LottoNumber expect = LottoNumber.valueOf(20);
+        LottoNumber actual = LottoNumber.valueOf(20);
+        assertThat(actual.equals(expect)).isTrue();
+    }
+
+    @DisplayName("다른 값을 가지는 로또 객체는 논리적으로 다름")
+    @Test
+    void equals_DifferentNumber() {
+        LottoNumber expect = LottoNumber.valueOf(20);
+        LottoNumber actual = LottoNumber.valueOf(21);
+        assertThat(actual.equals(expect)).isFalse();
+    }
+
+    @DisplayName("같은 값을 가지는 로또 객체가 같은 해시값을 가짐")
+    @Test
+    void hashCode_equalNumber() {
+        LottoNumber expect = LottoNumber.valueOf(20);
+        LottoNumber actual = LottoNumber.valueOf(20);
+        assertThat(actual.hashCode()).isEqualTo(expect.hashCode());
     }
 }
