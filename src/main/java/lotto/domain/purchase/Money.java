f00a52c8f48693ab51ed531da3e9f25286b9e47c
K.S.KIM <nullable@kakao.com>
2020-06-23 11:22:57 +0900

    feat: Money에 입력받은 만큼의 금액을 소비하는 메서드 추가

--- src/main/java/lotto/domain/purchase/Money.java
+++ src/main/java/lotto/domain/purchase/Money.java
@@ -1,16 +1,30 @@
 package lotto.domain.purchase;
 
-import lotto.exception.InvalidMoneyAmountException;
+import java.util.Objects;
 
 public class Money {
     private static final int MONEY_UNIT = 1000;
     private static final int MIN_MONEY_AMOUNT = 0;
 
-    private final int amount;
+    private int amount;
 
     public Money(final int amount) {
         validateAmount(amount);
         this.amount = amount;
+    }
+
+    public void spend(final Money spendMoney) {
+        validateMoneyAvailability(spendMoney);
+        this.amount -= spendMoney.amount;
+    }
+
+    private void validateMoneyAvailability(final Money spendMoney) {
+        if (this.amount < spendMoney.amount) {
+            throw new LackOfMoneyException("금액이 부족합니다. " +
+                    "보유금액: " + this.amount + ", " +
+                    "소모할 금액: " + spendMoney.amount
+            );
+        }
     }
 
     private void validateAmount(final int amount) {
@@ -25,12 +39,27 @@ public class Money {
     }
 
     private void validateAmountRange(final int amount) {
-        if (amount <= MIN_MONEY_AMOUNT) {
+        if (amount < MIN_MONEY_AMOUNT) {
             throw new InvalidMoneyAmountException("입력한 금액이 양수가 아닙니다: " + amount);
         }
     }
 
     public int getAmount() {
         return amount;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        final Money money = (Money)o;
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
--- src/test/java/lotto/domain/purchase/MoneyTest.java
+++ src/test/java/lotto/domain/purchase/MoneyTest.java
@@ -6,19 +6,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import lotto.exception.InvalidMoneyAmountException;
-
 class MoneyTest {
-    @DisplayName("로또 구입 금액을 입력하여 인스턴스를 생성한다.")
-    @ValueSource(ints = {1_000, 5_000, 10_000})
+    @DisplayName("로또 구입 금액을 입력하여 인스턴스를 생성")
+    @ValueSource(ints = {0, 1_000, 5_000, 10_000})
     @ParameterizedTest
     void constructor(final int amount) {
         assertThat(new Money(amount)).isInstanceOf(Money.class);
     }
 
-    @DisplayName("입력한 로또 구입 금액이 양수가 아닌 경우 예외가 발생한다.")
+    @DisplayName("입력한 로또 구입 금액이 0 또는 양수가 아닌 경우 예외가 발생")
     @Test
     void constructor_ZeroOrNegativeAmount_ExceptionThrown() {
         int amount = -1 * 1_000;
@@ -27,7 +26,7 @@ class MoneyTest {
                 .hasMessageContaining("입력한 금액이 양수가 아닙니다");
     }
 
-    @DisplayName("구입 금액이 1,000원 단위로 입력되지 않은 경우 예외가 발생한다.")
+    @DisplayName("구입 금액이 1,000원 단위로 입력되지 않은 경우 예외가 발생")
     @Test
     void constructor_MoneyUnitDivideRemainderIsOverZero_ExceptionThrown() {
         assertThatThrownBy(() -> new Money(10_100))
@@ -35,10 +34,34 @@ class MoneyTest {
                 .hasMessageContaining("입력한 금액이 1,000 단위로 나누어 떨어지지 않습니다");
     }
 
+    @DisplayName("입력받은 만큼의 돈을 소비")
+    @CsvSource(value = {"1000, 9000", "10000, 0"})
+    @ParameterizedTest
+    void spend(final int spendAmount, final int expectAmount) {
+        Money expect = new Money(expectAmount);
+        Money actual = new Money(10_000);
+        Money spendMoney = new Money(spendAmount);
+
+        actual.spend(spendMoney);
+
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @DisplayName("소비하고자 하는 양만큼의 금액이 없는 경우 예외 발생")
+    @Test
+    void spend_LackOfMoney_ExceptionThrown() {
+        Money money = new Money(1_000);
+        Money spendMoney = new Money(2_000);
+
+        assertThatThrownBy(() -> money.spend(spendMoney))
+                .isInstanceOf(LackOfMoneyException.class)
+                .hasMessageContaining("금액이 부족합니다");
+    }
+
     @DisplayName("생성된 money의 값을 확인")
     @Test
     void getAmount() {
-        Money money = new Money(20_000);
-        assertThat(money.getAmount()).isEqualTo(20_000);
+        Money money = new Money(10_000);
+        assertThat(money.getAmount()).isEqualTo(10_000);
     }
 }
