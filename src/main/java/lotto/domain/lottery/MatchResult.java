7bd1da2706471a2adb5eea37fe410b246a58d5e5
K.S.KIM <nullable@kakao.com>
2020-06-23 19:48:59 +0900

    feat: 수익률 계산 메서드 추가

--- src/main/java/lotto/domain/lottery/MatchResult.java
+++ src/main/java/lotto/domain/lottery/MatchResult.java
@@ -4,11 +4,23 @@ import java.util.Map;
 import java.util.Objects;
 
 public class MatchResult {
+    private static final int MAX_PERCENTAGE = 100;
     private final Map<Rank, Long> rankCount;
 
     public MatchResult(final Map<Rank, Long> rankCount) {
         Objects.requireNonNull(rankCount, "결과가 존재하지 않습니다.");
         this.rankCount = rankCount;
+    }
+
+    public long calculateProfitRate(final int totalSpendMoney) {
+        return calculateTotalPrize() * MAX_PERCENTAGE / totalSpendMoney;
+    }
+
+    private long calculateTotalPrize() {
+        return rankCount.entrySet()
+                .stream()
+                .mapToLong(entry -> entry.getKey().calculateTotalPrize(entry.getValue()))
+                .sum();
     }
 
     public Long getCount(final Rank rank) {
