be66f4aa9757befeb17cccf6b70baf80c4ef5919
K.S.KIM <nullable@kakao.com>
2020-06-23 11:49:10 +0900

    refactor: 테스트 클래스 접근 제어자 변경

--- src/test/java/lotto/domain/lottery/LottoTicketsTest.java
+++ src/test/java/lotto/domain/lottery/LottoTicketsTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LottoTicketsTest {
+class LottoTicketsTest {
     @DisplayName("여러장의 로또 티켓 생성")
     @Test
     void constructor() {
